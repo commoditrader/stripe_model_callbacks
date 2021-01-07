5438bda50cbf7f05692dd7d650eebf39c66acbc5
Robin Wunderlin <robin@commoditrader.com>
2021-01-07 15:37:00 +0100

    Fix method names

--- db/migrate/20210107120419_convert_stripe_discounts_coupon_redeem_by_int_to_timestamp.rb
+++ db/migrate/20210107120419_convert_stripe_discounts_coupon_redeem_by_int_to_timestamp.rb
@@ -4,11 +4,11 @@ class ConvertStripeDiscountsCouponRedeemByIntToTimestamp < ActiveRecord::Migrati
       stripe_discount.update_columns(coupon_redeem_by: Time.zone.at(stripe_discount.coupon_redeem_by_int)) # rubocop:disable Rails/SkipsModelValidations
     end
 
-    remove_colum :stripe_discounts, :coupon_redeem_by_int
+    remove_column :stripe_discounts, :coupon_redeem_by_int
   end
 
   def down
-    add_colum :stripe_discounts, :coupon_redeem_by_int
+    add_column :stripe_discounts, :coupon_redeem_by_int
 
     StripeDiscount.where.not(coupon_redeem_by: nil).each do |stripe_discount|
       stripe_discount.update_columns(coupon_redeem_by_int: stripe_discount.coupon_redeem_by.to_i) # rubocop:disable Rails/SkipsModelValidations
